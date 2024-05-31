868ba49f85f2e0e6907b15af75f7f680ecc81b32
123naim <mdnayemiqbal886@gmail.com>
2024-05-31 23:31:25 +0600

    lets check

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 1. Write a function to convert Celsius to Fahrenheit. The function should take a single argument, which is the temperature in Celsius.
+// 1. Write a function to convert Celsius to Fahrenheit. The function should take a single argument, which is the temperature in Celsius
 
 function celsiusToFahrenheit(celsius) {
     const fahrenheit = celsius * (9 / 5) + 32;
